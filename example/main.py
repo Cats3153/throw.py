c926ca32fbd014398d7e186e3af47a28b4871ed3
Cats <67523849+Cats3153@users.noreply.github.com>
2021-07-21 18:54:20 -0700

    add example

---
+++ example/main.py
@@ -0,0 +1,9 @@
+import subprocess
+import importlib
+
+res = subprocess.run(['node', 'suspicious.js'], capture_output=True)
+with open('sus.py', 'w') as suspicious_file:
+	suspicious_file.write(res.stdout.decode())
+
+import sus
+throw(Exception('suspicious')) # pog
