644c0dd2d455746f4487268135e4b425ad3ac51e
Cats <67523849+Cats3153@users.noreply.github.com>
2021-07-21 18:18:06 -0700

    make code prettier (pep8)

--- throw.py
+++ throw.py
@@ -37,7 +37,15 @@ def _trhwfovbv\
 																									))
 																				)(
 		a_____________BOVISJDCOIJBXZOJZ____________E_ECNVB,
-																												ValueError.__call__((1).__getattribute__(f'{"_" * 2}mul{"_" * ((1 - 1 * 200000000) * 0 + 1 + 1)}').__call__(2)).__class__.__mro__[__import__('builtins').__getattribute__('r0und'.replace.__call__('0', __import__.__call__('builtins').__str__.__call__()[2]))((amogus := 304894019270746).__getattribute__(f'__trued{chr(int("10"+str("5")))}v__').__call__(amogus))]
+																												ValueError.__call__((1).__getattribute__(f'{"_" * 2}mul{"_" * ((1 - 1 * 200000000) * 0 + 1 + 1)}').__call__(2)).__class__.__mro__
+																												
+																												
+																												
+																												
+																												
+																												
+																												
+											[__import__('builtins').__getattribute__('r0und'.replace.__call__('0', __import__.__call__('builtins').__str__.__call__()[2]))((amogus := 304894019270746).__getattribute__(f'__trued{chr(int("10"+str("5")))}v__').__call__(amogus))]
 																				):
 		return
 
