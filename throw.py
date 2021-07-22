584886e8e26cf5661d88d1b5f3ede26458df61b9
Cats <67523849+Cats3153@users.noreply.github.com>
2021-07-21 18:16:55 -0700

    Create throw.py

---
+++ throw.py
@@ -0,0 +1,83 @@
+def _trhwfovbv\
+(
+
+		\
+						a_____________BOVISJDCOIJBXZOJZ____________E_ECNVB			                    
+																																									):
+	"""
+																																fvbxoz;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+																				"""
+	if not __import__   .__call__('builtins')\
+																									.__getattribute__('{}{}{}{}{}{}{}{}{}{}'\
+																		.format(* list(tuple(map(lambda a___SD__FJJFVB____________: a___SD__FJJFVB____________, list \
+																		
+																		
+																		
+																		
+																		
+									(
+																		(
+																			'AAAAAAA'
+																		
+																		
+																		
+																		
+								'AAAA'
+			'AAA'
+																					'AAAAAAAAA' * 0
+																		).join(map(lambda _VBOSIBCOCOIC_: _VBOSIBCOCOIC_,
+											'isinstance')
+											
+									)
+					), 
+																							
+																					
+																												)
+												)
+																									))
+																				)(
+		a_____________BOVISJDCOIJBXZOJZ____________E_ECNVB,
+																												ValueError.__call__((1).__getattribute__(f'{"_" * 2}mul{"_" * ((1 - 1 * 200000000) * 0 + 1 + 1)}').__call__(2)).__class__.__mro__[__import__('builtins').__getattribute__('r0und'.replace.__call__('0', __import__.__call__('builtins').__str__.__call__()[2]))((amogus := 304894019270746).__getattribute__(f'__trued{chr(int("10"+str("5")))}v__').__call__(amogus))]
+																				):
+		return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+	raise (\
+
+
+						a_____________BOVISJDCOIJBXZOJZ____________E_ECNVB
+						
+						
+						
+						
+
+						
+						
+						
+						
+						
+						
+						
+						
+						
+															);
+
+
+throw =                                                                   \
+										_trhwfovbv
